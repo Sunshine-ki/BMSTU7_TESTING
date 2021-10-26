0b6c3b3e2c045604d2b2f03a7012f1336e59d714
Sunshine-ki <sukocheva.alis@mail.ru>
2021-10-26 21:00:43 +0300

    add tests for db

---
+++ Testing/DB/Facade.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using Moq;
+using PostSharp.Aspects;
+
+using Head;
+using db;
+
+using Testing.Builders;
+
+namespace Testing.BD
+{
+    public class FacadeGetTaskTests
+    {
+        private db.Task taskEmpty = null;
+
+        [Fact]
+        public void ShouldBeReturnTaskTest()
+        {
+            int id = 3;
+            var mustBeResult = new db.Task();
+            var mock = new Mock<db.IRepositoryTask>();
+            mock.Setup(facade => facade.GetTask(id)).Returns(mustBeResult);
+            var facade = new db.ConFacade(null, mock.Object, null);
+
+            var result = facade.GetTask(id);
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void ShouldBeTrowExceptionTaskNotFoundTest()
+        {
+            int id = 3;
+            var mock = new Mock<db.IRepositoryTask>();
+            mock.Setup(facade => facade.GetTask(id)).Returns(taskEmpty);
+            var facade = new db.ConFacade(null, mock.Object, null);
+
+            Assert.Throws<Exception>(() => facade.GetTask(id));
+        }
+    }
+}
